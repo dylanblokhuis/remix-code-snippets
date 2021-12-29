f30149086fb6f87857f92ba5758a3166c94cfab2
dylanblokhuis <dylanblokhuis3@hotmail.com>
2021-12-29 16:37:45 +0100

    init

---
+++ src/extension.ts
@@ -0,0 +1,113 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import createCodeSnippet, { FC_A, FC_L, FC_AL, FC_H, FC_HA, FC_HL, FC_HAL } from './snippets';
+
+interface Configuration extends vscode.WorkspaceConfiguration {
+	useLetInsteadOfConst: boolean
+	additionalCodeAfterImports: string
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "remix-code-snippets" is now active!');
+
+	vscode.languages.registerCompletionItemProvider(
+		{
+			language: 'typescriptreact',
+		},
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const configuration = vscode.workspace.getConfiguration("remix-code-snippets") as Configuration;
+				const statement = configuration.useLetInsteadOfConst ? "let" : "const";
+				const codeAfterImports = configuration.additionalCodeAfterImports
+					? `${configuration.additionalCodeAfterImports}\n`
+					: "";
+
+				return [
+					createCodeSnippet(
+						"rmx_fc_a",
+						"Functional component with action",
+						FC_A({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": ""
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_l",
+						"Functional component with loader",
+						FC_L({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": ""
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_al",
+						"Functional component with action and loader",
+						FC_AL({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": "",
+							"$4": "",
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_h",
+						"Functional component with handle",
+						FC_H({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": "",
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_ha",
+						"Functional component with handle and action",
+						FC_HA({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": "",
+							"$4": "",
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_hl",
+						"Functional component with handle and loader",
+						FC_HL({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": "",
+							"$4": "",
+						}
+					),
+					createCodeSnippet(
+						"rmx_fc_hal",
+						"Functional component with handle, action and loader",
+						FC_HAL({ statement, codeAfterImports }),
+						{
+							"${1:ComponentName}": "ComponentName",
+							"$2": "",
+							"$3": "",
+							"$4": "",
+							"$5": "",
+						}
+					),
+				];
+			}
+		},
+	);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
---
+++ src/snippets.ts
@@ -0,0 +1,138 @@
+import * as vscode from "vscode";
+
+interface SnippetOptions {
+  statement: "const" | "let"
+  codeAfterImports: string
+}
+
+export const FC_A = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { ActionFunction } from "remix"
+${codeAfterImports}
+export ${statement} action: ActionFunction = () => {
+  $3
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_L = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { LoaderFunction } from "remix"
+${codeAfterImports}
+export ${statement} loader: LoaderFunction = () => {
+  $3
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_AL = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { ActionFunction, LoaderFunction } from "remix"
+${codeAfterImports}
+export ${statement} action: ActionFunction = () => {
+  $3
+}
+
+export ${statement} loader: LoaderFunction = () => {
+  $4
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_H = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `export ${statement} handle = {
+  $3
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_HL = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { LoaderFunction } from "remix"
+${codeAfterImports}
+export ${statement} handle = {
+  $3
+}
+
+export ${statement} loader: LoaderFunction = () => {
+  $4
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_HA = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { ActionFunction } from "remix"
+${codeAfterImports}
+export ${statement} handle = {
+  $3
+}
+
+export ${statement} action: ActionFunction = () => {
+  $4
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export const FC_HAL = ({ statement, codeAfterImports }: SnippetOptions) =>
+  `import type { ActionFunction, LoaderFunction } from "remix"
+${codeAfterImports}
+export ${statement} handle = {
+  $3
+}
+
+export ${statement} action: ActionFunction = () => {
+  $4
+}
+
+export ${statement} loader: LoaderFunction = () => {
+  $5
+}
+
+export default function \${1:ComponentName}() {
+  return (
+    <div>$2</div>
+  )
+}`;
+
+export default function createCodeSnippet(
+  trigger: string,
+  label: string,
+  snippetString: string,
+  variables: {
+    [key: string]: string
+  }
+): vscode.CompletionItem {
+  const completionItem = new vscode.CompletionItem(trigger, vscode.CompletionItemKind.Snippet);
+  completionItem.insertText = new vscode.SnippetString(snippetString);
+  completionItem.detail = label;
+
+  let filledSnippetString = snippetString;
+  for (const [key, value] of Object.entries(variables)) {
+    filledSnippetString = filledSnippetString.replace(key, value);
+  }
+  const markdown = new vscode.MarkdownString();
+  markdown.appendCodeblock(filledSnippetString, "typescriptreact");
+  completionItem.documentation = markdown;
+
+  return completionItem;
+}
